99570e859511a62b2a70984943a377d61780f995
Iva Dimitrov <ivad1024@gmail.com>
2026-03-14 19:47:57 -0500

    fix

---
+++ character.js
@@ -0,0 +1,10 @@
+const readlineSync = require('readline-sync');
+let studentName = readlineSync.question('What is your name? ');
+console.log('Hi ' + studentName + '!');
+
+let studentName = "Michael Jackson";
+let firstInitial = studentNameName[0];
+let lastInitial = studentName[14];
+let outputSrting = "My name is " + studentName + " but you can call me " + firstInitial + lastInitial + ".";
+console.log(outputSrting);
+
